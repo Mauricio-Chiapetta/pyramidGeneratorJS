057e0e2ac0c97b2a8c285083aa909f5e4aabba8b
Mauricio Chiapetta <m.faisca1234@gmail.com>
2024-05-27 19:48:51 -0300

    first commit

---
+++ pyramid.js
@@ -0,0 +1,30 @@
+// // pyramid generator
+const character = "*";
+const count = 8;
+const rows = [];
+
+function padRow(rowNumber, RowCount) {
+    return " ".repeat(RowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(RowCount - rowNumber);
+
+}
+
+// TODO:use a different type of loop
+// for (let i = 1; i <= count; i++) {
+//     rows.push(padRow(i,count))
+// }
+
+// while (rows.length  < count) {
+// rows.push(padRow(rows.length + 1 ,count))
+// }
+
+// upsidedown pyramid
+for(let i = count;i>0;i--){
+    rows.push(padRow(i,count))
+}
+let result = "";
+
+for (const row of rows) {
+    result = result + "\n" + row;
+}
+console.log(result)
+
