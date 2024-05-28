a509be256168b41d4d90c4a01c1c6d1c0d90b612
Mauricio Chiapetta <m.faisca1234@gmail.com>
2024-05-27 22:10:56 -0300

    comentários

--- pyramid.js
+++ pyramid.js
@@ -1,7 +1,8 @@
 // // pyramid generator
 const character = "*";
-const count = 8;
+const count = 8;//contador começa em 8
 const rows = [];
+
 
 function padRow(rowNumber, RowCount) {
     return " ".repeat(RowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(RowCount - rowNumber);
@@ -9,18 +10,21 @@ function padRow(rowNumber, RowCount) {
 }
 
 // TODO:use a different type of loop
-// for (let i = 1; i <= count; i++) {
-//     rows.push(padRow(i,count))
-// }
+for (let i = 1; i <= count; i++) {
+    rows.unshift(padRow(i,count))//inverted pyramid
+    rows.push(padRow(i,count))//normal pyramid
+}
 
+// differents ways to use loops("while" and "for") to generate the pyramid
 // while (rows.length  < count) {
 // rows.push(padRow(rows.length + 1 ,count))
 // }
 
 // upsidedown pyramid
-for(let i = count;i>0;i--){
-    rows.push(padRow(i,count))
-}
+// for(let i = count;i>0;i--){
+//     rows.push(padRow(i,count))
+// }
+
 let result = "";
 
 for (const row of rows) {
